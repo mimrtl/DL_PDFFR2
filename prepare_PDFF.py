2496961a2f589ab6531aaaaffc9c5f2fc29c6b7a
Helena VanHemmen <vanhemmen@cn2.medphysics>
2020-06-11 11:33:56 -0500

    -m concatenate appending 48 slices aftr count=1

--- prepare_PDFF.py
+++ prepare_PDFF.py
@@ -12,7 +12,7 @@ out_ch = 3
 
 in_rows = 256
 in_col = 256
-data_folder = '/Users/helenavanhemmen/Desktop/Folder/MIMRTL/DLforPDFFandR2_Data/Ideal_data1p5'
+data_folder = '/data/data_mrcv2/MCMILLAN_GROUP/50_users/Helena/Ideal_data1p5'
 
 
 def prepare():
@@ -32,7 +32,11 @@ def prepare():
 
         
             img_idata = curr_input['img_idata']
+            print('img_idata: ')
+            print(img_idata.shape)
             img_ref = curr_input['img_ref']
+            print('img_ref: ')
+            print(img_ref.shape)
 
             in_slices = img_idata.shape[2]
 
@@ -40,21 +44,28 @@ def prepare():
             output = np.zeros([in_rows,in_col,in_slices,out_ch])
 
             curr_in = img_idata[:,:,:,0:2]
+            print('curr_in: ')
             print(curr_in.shape)
             curr_out = img_ref[:,:,:,:]
+            print('curr_out: ')
             print(curr_out.shape)
 
             if input_count == 1:
                 input_echo1 = curr_in
                 output = curr_out
-            else:
+            elif input_count != 1:
                 input_echo1 = np.concatenate((input_echo1,curr_in),axis=2)
                 output = np.concatenate((output,curr_out),axis=2)
 
             print('count = {}'.format(input_count))
-
+            
             if input_count == 120:
                 break
+            
+            input_echo1 = np.swapaxes(input_echo1,2,0)
+            output = np.swapaxes(output,2,0)
+            print(input_echo1.shape)
+            print(output.shape)
 
             np.save('input_echo1.npy',input_echo1)
             np.save('output.npy',output)
